5d40ed159829d9ebf795962e62bd0ab4decd3199
Hongzheng Li <hongzheng@cs.wisc.edu>
2025-02-24 19:15:27 -0600

    feat: enhance Timer with elapsed time tracking and control methods

--- Timer.h
+++ Timer.h
@@ -8,12 +8,15 @@ struct Timer
 {
     using clock_t = std::chrono::high_resolution_clock;
     using time_point_t = std::chrono::time_point<clock_t>;
+    using elapsed_time_t = std::chrono::duration<double, std::milli>;
 
     time_point_t mStartTime;
     time_point_t mStopTime;
+    elapsed_time_t mElapsedTime;
 
     void Start()
     {
+        mElapsedTime = elapsed_time_t::zero();
         mStartTime = clock_t::now();
     }
 
@@ -22,6 +25,27 @@ struct Timer
         mStopTime = clock_t::now();
         std::chrono::duration<double, std::milli> elapsedTime = mStopTime - mStartTime;
         std::cout << "[" << msg << elapsedTime.count() << "ms]" << std::endl;
+    }
+
+    void Reset()
+    {
+        mElapsedTime = elapsed_time_t::zero();
+    }
+    
+    void Restart()
+    {
+        mStartTime = clock_t::now();
+    }
+
+    void Pause()
+    {
+        mStopTime = clock_t::now();
+        mElapsedTime += mStopTime-mStartTime;
+    }
+
+    void Print(const std::string& msg)
+    {
+        std::cout << "[" << msg << mElapsedTime.count() << "ms]" << std::endl;
     }
     
 };
